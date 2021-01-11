7ea76c2acde10f9ce2374f6e4ee37d2f3dd77f42
Artur Mostowski <artur.mostowski@currency-one.com>
2021-01-11 20:34:01 +0100

    front | basic redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Provider, connect } from 'react-redux'
 
-function App() {
+function App({ count, handleIncrementClick, handleDecrementClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Helloworld React & Redux! {count}</h1>
+        <button onClick={handleDecrementClick}>Decrement</button>
+        <button onClick={handleIncrementClick}>Increment</button>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
+}
+const mapStateToProps = (state) => {
+  return {
+    count: state,
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleIncrementClick: () => dispatch({ type: 'INCREMENT' }),
+    handleDecrementClick: () => dispatch({ type: 'DECREMENT' }),
+  }
+}
+const Container = connect(mapStateToProps, mapDispatchToProps)(App)
+export default Container
---
+++ frontend/src/reducers.js
@@ -0,0 +1,12 @@
+const mainReducer = function (state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+module.exports = { mainReducer }
