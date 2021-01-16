837329497a11e8f1a7c0c9e5181df0a7640679b5
Artur Mostowski <artur.mostowski@currency-one.com>
2021-01-16 18:00:41 +0100

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg'
 import './App.css'
-import { connect } from 'react-redux'
 import Inputs from './inputs'
-function App({ count, handleIncrementClick, handleDecrementClick }) {
+export default function App() {
   return (
     <div className='App'>
       <Inputs></Inputs>
     </div>
   )
 }
-const mapStateToProps = (state) => {
-  return {
-    count: state,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    handleIncrementClick: () => dispatch({ type: 'INCREMENT' }),
-    handleDecrementClick: () => dispatch({ type: 'DECREMENT' }),
-  }
-}
-const Container = connect(mapStateToProps, mapDispatchToProps)(App)
-export default Container
