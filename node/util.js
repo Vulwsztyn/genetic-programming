0c64635fc1eaf4eb7b19e9b4f8c42608631e2486
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2021-01-22 17:29:13 +0000

    Deploying to gh-pages from  @ c348e161b88bc761ddbe57081afb089e4f9233f3 🚀

--- node/util.js
+++
@@ -1,8 +0,0 @@
-function stringifySpecimen(specimen) {
-  return specimen.type === 'T'
-    ? `${specimen.value}`
-    : `${specimen.name}(${specimen.children ? specimen.children.map(stringifySpecimen).join(',') : ''})`
-}
-module.exports = {
-  stringifySpecimen,
-}
